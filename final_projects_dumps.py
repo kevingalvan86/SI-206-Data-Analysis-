5169724f2c45d5946b6aa3f8c580bab83ee4f9d9
iss-mobileuser <iss-mobileuser@adsroot.itcs.umich.edu>
2020-04-26 20:31:57 -0400

    SELECT with Join

--- final_projects_dumps.py
+++ final_projects_dumps.py
@@ -4,5 +4,13 @@ import sqlite3
 def main():
     conn = sqlite3.connect('Project.db') 
     c = conn.cursor() 
+    c.execute('''SELECT Country.Name, Region.Name, Covid.Cases, Covid.Deaths, Country.Population
+    Google.Value FROM Covid JOIN Country JOIN Google JOIN Region ON
+    (Covid.Country = Country.id AND 
+    Google.Country = Covid.Country AND 
+    Region.id =
+     Covid.
+     Region)''')
+    print(c.fetchall())
 if __name__ == '__main__':
     main()
